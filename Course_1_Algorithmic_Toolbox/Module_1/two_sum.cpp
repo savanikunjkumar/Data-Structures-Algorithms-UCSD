0b2d52f0347bb70c15fcf11c2a7282820bd8493f
savanikunjkumar <savani.kunjkumar@gmail.com>
2026-04-21 15:29:36 +0530

    Create two_sum.cpp

---
+++ Course_1_Algorithmic_Toolbox/Module_1/two_sum.cpp
@@ -0,0 +1,27 @@
+/*
+ * Author: Kunjkumar Savani
+ * Course: Algorithmic Toolbox
+ * Module: 1st
+ * Assignment: Sum of Two Digits
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a, b;
+    
+   
+    if (cin >> a >> b) {
+        // Output their sum
+        cout << a + b << "\n";
+    }
+
+    return 0;
+}
